9dd71195648547d59e30aeb0177a61cc431ad3b2
David Wilson <dws1985@hotmail.com>
2017-01-22 19:21:13 +0100

    Add files via upload

---
+++ sim-mut.py
@@ -0,0 +1,222 @@
+#!/usr/bin/python
+#
+#
+# This program simulates a mutagenesis. It takes a DNA sequence and introduces mutations.
+# 
+# PARAMETERS EXPLANATION:
+# 
+# -nbr (integer): Total number of mutations. It must be an integer. Absolute value. The ratio between
+#       the number of mutations asked to create and the length of the fasta sequence is limited to 0.02.
+# 
+# -mod [d, e, li]: Mutation mode. 'd': Any base can be mutated and any of the three alternative bases
+#       can be the variant, like in genetic drift; 'e': Only CG > AT transitions are created, as caused by
+#       ethyl methanesulfonate (EMS); 'li': The sequence of a large insertion provided by the user is inserted
+#       at random positions.
+# 
+# -con: Source of the contig sequence to mutate. It must be in fasta format. It only supports fasta files
+#       with a single contig. It does not support sequences that contain bases not stored in
+#       'options_basecall_errors'.
+# 
+# -ins: Source of the insertion sequence. It must be in fasta format and contain a single contig.
+#       Only required in mode 'large insertions'. 
+# 
+# -out: Name of the file (or relative path) where mutated fasta data will be written
+# 
+# 
+# 2016 - David Wilson - dws1985@hotmail.com
+
+
+import argparse, os, shutil
+from random import randint
+
+
+# Parse command arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('-nbr', action="store", dest='total_nbr_mutations', type=int, required=True)
+parser.add_argument('-mod', action="store", dest='mutator_mode',
+choices=set(('d','e','li')), required=True) #Choose between d, e, li (d = genetic drift SNP mutations, e = EMS SNP mutations GC>AT, li = long insertion (t-dna, transposon))
+parser.add_argument('-con', action="store", dest='contig_source', required=True)
+parser.add_argument('-ins', action="store", dest='insertion_source')
+parser.add_argument('-out', action="store", dest='output')
+
+args = parser.parse_args()
+
+total_nbr_mutations = int(args.total_nbr_mutations)
+mutator_mode = args.mutator_mode   
+contig_source = args.contig_source
+insertion_source = args.insertion_source
+output = args.output
+
+if mutator_mode == 'li' and insertion_source is None:
+	quit('Quit. Selected mode is "large insertions" but no insertion sequence source was provided. See program description.')		
+
+# Process 'args.output_file_1'
+if '/' in output:
+	slash_pos = output.rfind('/')
+	output_folder = output[:slash_pos]
+	output_file_1 = output[slash_pos+1:]
+	if os.path.exists(output_folder): # If the outpurt directory already exists, remove first
+		shutil.rmtree(output_folder)
+	os.makedirs(output_folder)
+
+
+# Function to parse fasta file (based on one of the Biopython IOs)
+def read_fasta(fp):
+	name, seq = None, []
+	for line in fp:
+		line = line.rstrip()
+		if line.startswith('>'):
+			if name: yield (name, ''.join(seq))
+			name, seq = line, []
+		else:
+			seq.append(line)
+	if name: yield (name, ''.join(seq))
+
+
+# Function to divide a long string ('data') into chunks of defined length ('batch_size')
+def batch_gen(data, batch_size):
+	for i in range(0, len(data), batch_size):
+		yield data[i:i+batch_size]
+
+
+# Read contig fasta file
+with open(contig_source) as fp:
+	for name_contig, seq_contig in read_fasta(fp):
+		pass
+
+# Calculate contig length
+contig_length = len(seq_contig)
+
+# Convert contig sequence to uppercase
+seq_contig_uppercase = seq_contig.upper()
+
+# Check that ratio (nbr of mutations / contig length) is below threshold and stop program if necessary
+ratio_ml = float(total_nbr_mutations) / float(contig_length)
+if ratio_ml > 0.02:
+	quit('Quit. The ratio (nbr of mutations / contig length) is > 0.02')
+
+# Randomly choose mutation positions, add them to a list, and finally sort the list
+all_mut_pos = list() # Will store the list of mutated positions
+
+iter = 0
+while iter < total_nbr_mutations:
+	mut_pos = randint(1, contig_length - 2) # I deliberately exclude first and last nucleotides
+	if mut_pos not in all_mut_pos:
+		if mutator_mode == 'd' or mutator_mode == 'li': # If mode is drift, any base can be mutated
+			iter +=1
+			all_mut_pos.append(mut_pos)
+		if mutator_mode == 'e': # If mode is EMS, ony G and A can be mutated
+			wt_base = seq_contig_uppercase[mut_pos:mut_pos+1]
+			if wt_base == 'G' or wt_base == 'C':
+				iter +=1
+				all_mut_pos.append(mut_pos)
+
+all_mut_pos.sort()
+
+
+# If mode is SNPs (drift or EMS), use list 'all_mut_pos' and determine the mutant allele at each position
+if mutator_mode == 'd' or mutator_mode == 'e':
+	all_mut_info = list() # Will store mutated positions, wt base, and mut base
+
+	# Iterate over list, determine the wt base for each position in list, and randomly choose a mt base
+	for i in all_mut_pos:
+		wt_base = seq_contig_uppercase[i:i+1]
+
+		if mutator_mode == 'd': # If mode is drift, the mutation can be any base
+			options_drift = {'A': ['T', 'C', 'G'], 'T': ['C', 'G', 'A'], 'C': ['G', 'A', 'T'], 'G': ['A', 'T', 'C']}
+			try:
+				possible_mt_bases = options_drift[wt_base] # Lookup the alterantive bases of "wt_base" in "options_drift"
+				mt_base = possible_mt_bases[randint(0, 2)] # Randomly pick between the possible mut bases
+			except KeyError:
+				mt_base = 'N'
+
+		if mutator_mode == 'e': # If mode is ems, only GC>AT possible
+			options_ems = {'G': 'A', 'C': 'T'}
+			try:
+				mt_base = options_ems[wt_base]
+			except KeyError:
+				mt_base = 'N'
+
+		mut_info = (i, wt_base, mt_base) 
+		all_mut_info.append(mut_info)
+	
+	# Iterate over all_mut_info list, mutate the wt sequence, and write the mutant version to file	
+	mutant_seq_contig = list(seq_contig_uppercase) # I use a list because python strings do not support item assignment
+		
+	for mut_info in all_mut_info:
+		# Write to info file
+		mutant_seq_contig[mut_info[0]] = mut_info[2] # For each mutation in list "all_mut_info", appropriately substitute in list "mutant_seq"
+	
+	output2 = open(output_folder + '/' + output_file_1, 'w')
+	output2.write(name_contig + '__mutated')
+	
+	# Write to file a small chunk of the contig sequence in each line
+	mutant_seq_contig = ''.join(mutant_seq_contig)
+	for chunk in batch_gen(mutant_seq_contig, 80):
+		output2.write('\n' + chunk)
+	
+	output2.close()
+		
+		
+if mutator_mode == 'li':
+	# How insertion positions are handled:
+	#  ATCG     .ATCG     A.TCG     AT.CG     ATC.G     ATCG.
+	#  01234    0          1          2          3          4
+
+	mutant_seq_contig = ''
+
+	# Read insertion fasta file
+	with open(insertion_source) as fp:
+		for name_ins, seq_ins in read_fasta(fp):
+			#print (name_ins, seq_ins)
+			pass
+	
+	# Create a new list that contains the insertion points and the beggining and end coordinates 
+	all_mut_pos_plus_ends = list(all_mut_pos)
+	
+	all_mut_pos_plus_ends.insert(0, 0) # Add the value "0" at position 0 of the list
+	all_mut_pos_plus_ends.append(len(seq_contig)) # Add the last base of the contig to the end of the list 
+	
+	# Iterate over list "all_mut_pos_plus_ends", take the coord values and substring the contig sequence, and build the mutant sequence
+	iter_ins = 0
+	while iter_ins < len(all_mut_pos_plus_ends):
+		try:
+			fragment = seq_contig_uppercase[all_mut_pos_plus_ends[iter_ins]:all_mut_pos_plus_ends[iter_ins + 1]]
+			#print fragment_len
+			mutant_seq_contig += fragment
+			if iter_ins < len(all_mut_pos_plus_ends) - 2:
+				mutant_seq_contig += seq_ins
+		except IndexError:
+			pass
+		
+		iter_ins +=1
+	
+	output2 = open(output_folder + '/' + output_file_1, 'w')
+	output2.write(name_contig + '__mutated')
+	
+	# Write to file a small chunk of the contig sequence in each line
+	for chunk in batch_gen(mutant_seq_contig, 80):
+		output2.write('\n' + chunk)
+		
+	output2.close()	
+
+
+# Iterate over list "all_mut_info" and create file with info of mutations
+output1 = open(output_folder + '/info_all_mutations.txt', 'w')
+
+# If mode is SNPs, write header with 3 columns and use list "all_mut_info".
+if mutator_mode == 'd' or mutator_mode == 'e':
+	output1.write('position\twt_base\tmt_base\n')
+	for mut_info in all_mut_info:
+		pos = int(mut_info[0]) + 1
+		output1.write(str(pos) + '\t' + str(mut_info[1]) + '\t' + str(mut_info[2]) + '\n')
+
+# If mode is large insertions, write name of inserted sequence, single column header, and just use list "all_mut_pos".
+if mutator_mode == 'li':
+	output1.write('name of inserted sequence: ' + name_ins[1:] + '\n\npositions of insertions\n')
+	for mut_pos in all_mut_pos:
+		output1.write(str(mut_pos) + '\n')
+output1.close()
+
+
+print 'Done!'
---
+++ sim-recsel.py
@@ -0,0 +1,275 @@
+#!/usr/bin/python
+# 
+# 
+# This program simulates the F2 offspring of a cross between two different isogenic lines and the
+# selection of individuals with a particular trait. The original purpose was to create mapping
+# populations. It takes two chromosomes from Arabidopsis thaliana and creates recombinant versions.
+# The recombination frequency distribution is based in Salome et al 2012, Heredity (2012) 108,
+# 447-455; doi:10.1038/hdy.2011.95. (The program can be modified to be used with other species
+# mainly by modifying the list(s) 'chr_xo_freq'). The positions of the crossovers are random.
+# The program then selects the recombinant chromosomes based on whether they carry or not a given
+# mutation selected by the user (the phenotype-causing mutation).
+# 
+# PARAMETERS EXPLANATION:
+# 
+# -outdir: Name of directory relative to current location where to place the recombinant chromosomes
+# 		created.
+# 
+# -achr (integer) [1, 2, 3, 4, 5]: Chromosome number from Arabidopsis thaliana. Used to apply the
+# 		recombination frequency distribution corresponding to that particular chromosome. It must match
+# 		the chromosomes provided in 'parmut' and 'parpol'.
+# 
+# -parmut: Parental A. In modes 'r', 'd', and 'di', this parental must be the mutant parental. It
+# 		must be a fasta-formatted file with a single contig. The length of the contig must be equal to the
+# 		length of -parpol.
+# 
+# -parpol: Parental B. Polymorphic to parental A. In modes 'r', 'd', and 'di', this parental must
+# 		be the non-mutant parental. It must be a fasta-formatted file with a single contig. The length of
+# 		the contig must be equal to the length of -parmut.
+# 
+# -mutapos (integer): Position of the causal mutation in -parmut.
+# 
+# -mutbpos (integer): Position of the causal mutation in -parpol. Only required if mode is 'dr'.
+# 
+# -smod [r, d, di, dr]: Selection mode. 'r': Recessive mutation and selection of the mutant phenotype
+# 		(recessive phenotypic class); 'd': Dominant mutation and selection of the mutant phenotype (dominant
+# 		phenotypic class); 'di': Dominant mutation and selection of the wild type phenotype (recessive
+# 		phenotypic class); 'dr': Two recessive mutations and selection of the double mutant phenotype.
+# 		In the first three modes, the causal mutation is provided in parental -parmut. In the last mode,
+# 		each causal mutation is provided in a different parental.
+# 
+# -nrec (integer): Number of recombinant chromosomes to create. This number corresponds to the number
+# 		of chromosomes after the selection has been performed. 
+#
+# 
+# 2016 - David Wilson - dws1985@hotmail.com
+
+
+
+import argparse, os, shutil
+from random import randint
+
+
+# Parse command arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('-outdir', action="store", dest='out_dir', required=True)
+parser.add_argument('-achr', action="store", dest='at_chr', 
+type=int, required=True, choices=set((1,2,3,4,5))) # Choose between 1, 2, 3, 4, 5
+parser.add_argument('-parmut', action="store", dest='parental_a_mutant', required=True) #mutated genome
+parser.add_argument('-parpol', action="store", dest='parental_b_polymorphic', required=True) #polymorphic genome
+parser.add_argument('-mutapos', action="store", dest='mut_a_pos', type=int, required=True)
+parser.add_argument('-mutbpos', action="store", dest='mut_b_pos', type=int)
+parser.add_argument('-smod', action="store", dest='selection_mode',
+required=True, choices=set(('r','d','di','dr'))) #Choose between...
+parser.add_argument('-nrec', action="store", dest='nbr_rec_chrs', type=int, required=True)
+args = parser.parse_args()
+
+out_dir = args.out_dir
+at_chr = int(args.at_chr)
+parental_a_mutant = args.parental_a_mutant
+parental_b_polymorphic = args.parental_b_polymorphic
+mut_a_pos = args.mut_a_pos
+mut_b_pos = args.mut_b_pos
+selection_mode = args.selection_mode # "r" = recessive, "d" = dominant mt-phe, "di" = dominant wt-phe, "dr" = double recessive
+nbr_rec_chrs = args.nbr_rec_chrs
+
+if selection_mode == 'dr' and mut_b_pos is None:
+	quit('Quit. Selected mode is "double recessive" but no position for mutation b was provided. See program description.')		
+
+
+# Function to parse fasta files
+def read_fasta(fp):
+	name, seq = None, []
+	for line in fp:
+		line = line.rstrip()
+		if line.startswith('>'):
+			if name: yield (name, ''.join(seq))
+			name, seq = line, []
+		else:
+			seq.append(line)
+	if name: yield (name, ''.join(seq))
+
+
+# Function to create a recombinant chromosome from two parental chromosomes and
+# the list 'crossover_positions' with XO positions.
+# I decide to group this code in function to be used in the different selection modes
+def create_rec_seq():
+	rec_chr = ''
+	for key,val in enumerate(crossover_positions):	
+		if starting_parental == 0:
+			if key % 2 == 0:
+				try: rec_chr += seq_parental_a[crossover_positions[key]:crossover_positions[key+1]]
+				except IndexError: pass
+			if key % 2 != 0:
+				try: rec_chr += seq_parental_b[crossover_positions[key]:crossover_positions[key+1]]
+				except IndexError: pass
+		if starting_parental == 1:
+			if key % 2 == 0:
+				try: rec_chr += seq_parental_b[crossover_positions[key]:crossover_positions[key+1]]
+				except IndexError: pass
+			if key % 2 != 0:
+				try: rec_chr += seq_parental_a[crossover_positions[key]:crossover_positions[key+1]]
+				except IndexError: pass					
+	return rec_chr
+
+# Function to divide a long string ('data') into chunks of defined length ('batch_size')
+def batch_gen(data, batch_size):
+	for i in range(0, len(data), batch_size):
+		yield data[i:i+batch_size]
+
+# Function to create Fasta file and write the sequence of a recombinant chromosome to it.
+def create_rec_chr_file():
+	output_file = open(out_dir + '/rec_chr_' + str(iter1 + 1) + '.fa', 'w')
+	output_file.write('>rec_chr_' + str(iter1 + 1))
+	for chunk in batch_gen(rec_chr, 80): # Write to file a small chunk of the contig sequence in each line
+		output_file.write('\n' + chunk)
+	output_file.close()
+
+
+# Create a subdirectory to place the recombinant chromosomes. If it already exists, remove it first
+if os.path.exists(out_dir):
+	shutil.rmtree(out_dir)
+os.makedirs(out_dir)
+
+
+# Read fasta files of parentals
+with open(parental_a_mutant) as fp:
+	for name_parental_a, seq_parental_a in read_fasta(fp):
+		#print(name_parental_a, seq_parental_a)
+		pass
+
+with open(parental_b_polymorphic) as fp:
+	for name_parental_b, seq_parental_b in read_fasta(fp):
+		#print(name_parental_b, seq_parental_b)
+		pass
+
+# Check that both parentals have identical length. If not, quit.
+if len(seq_parental_a) != len(seq_parental_b):
+	quit('Quit. The lengths of the two parental sequences provided are not identical')
+
+# Determine length of provided chromosome
+chr_len = len(seq_parental_a)
+
+# Define recombination frequencies of chromosomes in Arabidopsis.
+# Data from Salome et al 2012, Heredity (2012) 108, 447-455; doi:10.1038/hdy.2011.95
+# In each sublist [x,y,z]:
+#	y - x = Percentage of chromosomes
+#	z = Number of crossover events
+#	x and y: Used to compare with a random number [1, 100] to simulate chromosomes
+#				following the frequencies of crossover events published by
+#				Salome et al 2012.
+#	x = left interval (open)
+#	y = right interval (closed)
+
+if at_chr == 1:
+	chr_xo_freq = [[0,14,0],[14,45,1],[45,78,2],[78,93,3],[93,98,4],[98,100,5]]
+if at_chr == 2:
+	chr_xo_freq = [[0,26,0],[26,68,1],[68,93,2],[93,98,3],[98,99,4],[99,100,5]]
+if at_chr == 3:
+	chr_xo_freq = [[0,20,0],[20,59,1],[59,87,2],[87,97,3],[97,99,4],[99,100,5]]
+if at_chr == 4:
+	chr_xo_freq = [[0,24,0],[24,67,1],[67,92,2],[92,98,3],[98,99,4],[99,100,5]]
+if at_chr == 5:
+	chr_xo_freq = [[0,16,0],[16,50,1],[50,81,2],[81,95,3],[95,99,4],[99,100,5]]
+
+
+# Create a defined number of recombinant chromosomes 
+iter1 = 0
+while iter1 < nbr_rec_chrs:
+	
+	# Reset variables to 0 ('does not contain the mutation') at the beggining of each loop
+	chr_carries_mutation_a = False
+	chr_carries_mutation_b = False
+
+	# Randomly choose which of the two parentals the program will choose as "seed".
+	# The probabilities are 0.5/0.5. Use later.
+	starting_parental = randint(0, 1) # 0: start with mutated (parmut) genome, 1: start with polymorphic (parpol) genome
+	
+	# Create a radom number and compare it to the XO frequency table of the user-chosen chr 
+	# The XO frequency table represents the frequency of each number of XOs in a chromosome
+	# By comparing a series of random numbers with this table, it can be created a series of XOs
+	# numbers that follow exactly the real frequencies
+	rand_nbr = randint(1, 100) # 100 different options (values 0 and 100 are included)
+	for i in chr_xo_freq:
+		if rand_nbr > i[0] and rand_nbr <= i[1]:
+			nbr_crossovers = i[2] # This is the number of XOs the current rec chr will have 
+	
+	# Randomly create the genomic position of each XO event
+	iter2 = 0
+	crossover_positions = list()
+	while iter2 < nbr_crossovers:
+		crossover_positions.append(randint(1, chr_len))
+		iter2 +=1
+	
+	# Add the beggining and end coordinates of the chromosome to the list 'crossover_positions'
+	crossover_positions.append(0)
+	crossover_positions.append(chr_len)
+	crossover_positions.sort()
+	
+	# Determine if the resulting recombinant chr carries the primary (a) causal mutation
+	for key,val in enumerate(crossover_positions):
+		
+		if starting_parental == 0 and key % 2 == 0:
+			if mut_a_pos > crossover_positions[key] and mut_a_pos <= crossover_positions[key+1]:
+				chr_carries_mutation_a = True
+		
+		if starting_parental == 1 and key % 2 != 0:
+			if mut_a_pos > crossover_positions[key] and mut_a_pos <= crossover_positions[key+1]:
+				chr_carries_mutation_a = True
+	
+	# Determine if the resulting recombinant chr carries the secondary (b) causal mutation
+	for key,val in enumerate(crossover_positions):
+		
+		if starting_parental == 0 and key % 2 != 0:
+			if mut_b_pos > crossover_positions[key] and mut_b_pos <= crossover_positions[key+1]:
+				chr_carries_mutation_b = True
+
+		if starting_parental == 1 and key % 2 == 0:
+			if mut_b_pos > crossover_positions[key] and mut_b_pos <= crossover_positions[key+1]:
+				chr_carries_mutation_b = True
+
+	
+	# Chromosome selection
+	# If recombinant chr contains the desired mutation(s), execute 'create_rec_seq()'
+	# and 'create_rec_chr_file()' functions.
+	# The first function creates a Fasta file using the info in 'crossover_positions', and
+	# the second writes the output to a file.
+	
+	# Select all chromosomes that carry mutation A (all phenotypically mutant plants)
+	if selection_mode == 'r':
+		if chr_carries_mutation_a == True:
+			rec_chr = create_rec_seq()
+			create_rec_chr_file()
+			iter1 +=1
+	
+	# Select all chromosomes that carry mutation A and also some that do not, so the final
+	# proportion of chrs that carry the mutation is 0.67 (all phenotyoically mutant plants).
+	# This happens when selecting mutants with dominant mutation based on phenotype.
+	if selection_mode == 'd':
+		if chr_carries_mutation_a == True:
+			rec_chr = create_rec_seq()
+			create_rec_chr_file()
+			iter1 +=1
+		else:
+			chr_filtering_threshold = randint(1,100)
+			if chr_filtering_threshold > 50:
+				rec_chr = create_rec_seq()
+				create_rec_chr_file()
+				iter1 +=1
+	
+	# Select all chromosomes that do not carry mutation A (all phenotypically wild type plants)
+	if selection_mode == 'di':
+		if chr_carries_mutation_a == False:
+			rec_chr = create_rec_seq()
+			create_rec_chr_file()
+			iter1 +=1
+	
+	# Select all chromosomes that carry mutations A and B (all phenotipically double recessive mutants)
+	if selection_mode == 'dr':
+		if chr_carries_mutation_a == True and chr_carries_mutation_b == True:
+			rec_chr = create_rec_seq()
+			create_rec_chr_file()
+			iter1 +=1
+	
+
+print 'Done!'
